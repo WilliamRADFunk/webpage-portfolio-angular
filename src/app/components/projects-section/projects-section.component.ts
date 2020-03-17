71ba4e055f6d373b65e43f405d2e47af9796e90c
WilliamRADFunk <contact@WILLIAMRADFUNK.com>
2020-03-16 21:49:21 -0700

    Sets up the accordion for projects.

--- src/app/components/projects-section/projects-section.component.ts
+++ src/app/components/projects-section/projects-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { applications } from '../../../assets/base/categories.js';
 
 @Component({
   selector: 'app-projects-section',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects-section.component.scss']
 })
 export class ProjectsSectionComponent implements OnInit {
+  public activeId: string = '';
+  public applications: { html: string; name: string; tags: string[], year: number; }[] = applications;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleAccordian(e: { panelId: string }): void {
+    this.activeId = e.panelId;
+    console.log('event', e);
   }
 
 }
